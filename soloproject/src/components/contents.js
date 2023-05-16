501e73eb322c0eb39307f5ab4805b48c67065bd1
weed7968 <tlstjdcjf980809@gmail.com>
2023-05-16 13:42:09 +0900

    컨텐츠 CSS 수정

--- soloproject/src/components/contents.js
+++ soloproject/src/components/contents.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+const Content = styled.div`
+  margin-top: 30px;
+`;
+
 const Image = styled.img`
   width: 350px;
   height: 250px;
@@ -10,48 +14,72 @@ const Title = styled.h2`
   font-size: 16px;
 `;
 
+const Discount = styled.span`
+  color: #452cdd;
+  font-size: 16px;
+  font-weight: 1000;
+  float: right;
+`;
+
+const Price = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  float: right;
+`;
+
+const Follow = styled.span`
+  font-size: 16px;
+  font-weight: 1000;
+  float: right;
+`;
+
+const Follower = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  float: right;
+`;
+
 const Contents = ({ item }) => {
   switch (item.type) {
     case "Exhibition":
       return (
-        <div>
+        <Content>
           <Image src={item.image_url} />
           <Title>{item.title}</Title>
           <div>{item.sub_title}</div>
-        </div>
+        </Content>
       );
-      break;
     case "Brand":
       return (
-        <div>
+        <Content>
           <Image src={item.brand_image_url} />
-          <Title>{item.brand_name}</Title>
-          <div>관심 고객수</div>
-          <div>{item.follower}</div>
-        </div>
+          <Title>
+            {item.brand_name}
+            <Follow>관심 고객수</Follow>
+          </Title>
+          <Follower>{item.follower.toLocaleString()}</Follower>
+        </Content>
       );
-      break;
     case "Category":
       return (
-        <div>
+        <Content>
           <Image src={item.image_url} />
           <Title>#{item.title}</Title>
-        </div>
+        </Content>
       );
-      break;
     case "Product":
       return (
-        <div>
+        <Content>
           <Image src={item.image_url} />
-          <Title>{item.title}</Title>
-          <div>{item.discountPercentage}%</div>
-          <div>{item.price}</div>
-        </div>
+          <Title>
+            {item.title}
+            <Discount>{item.discountPercentage}%</Discount>
+          </Title>
+          <Price>{Number(item.price).toLocaleString()}원</Price>
+        </Content>
       );
-      break;
     default:
       return <div>error</div>;
-      break;
   }
 };
 
