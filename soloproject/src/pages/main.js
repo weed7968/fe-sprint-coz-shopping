b05979be2188fb9f5236d3b977cbb6d76e08bfeb
weed7968 <tlstjdcjf980809@gmail.com>
2023-05-17 12:02:15 +0900

    메인페이지 구현

--- soloproject/src/pages/main.js
+++ soloproject/src/pages/main.js
@@ -5,14 +5,26 @@ const Maincont = styled.main`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  margin-bottom: 10px;
 `;
 
-const Main = ({ data }) => {
-  const list = data.map((item) => {
-    return <Contents item={item} />;
-  });
+const Title = styled.h2``;
 
-  return <Maincont>{list}</Maincont>;
+const Main = ({ data }) => {
+  const list = data
+    .map((item) => {
+      return <Contents item={item} />;
+    })
+    .slice(0, 4);
+
+  return (
+    <>
+      <Title>상품 리스트</Title>
+      <Maincont>{list}</Maincont>
+      <Title>북마크 리스트</Title>
+      <Maincont>{list}</Maincont>
+    </>
+  );
 };
 
 export default Main;
